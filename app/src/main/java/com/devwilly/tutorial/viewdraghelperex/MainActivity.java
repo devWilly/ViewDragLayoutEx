174fc3cddde2bee0bc50bc3c3da8a0ec0fdaccd9
Willy Chang <ms70831@gmail.com>
2017-03-12 00:30:44 +0800

    create two fragment in my main activity

--- app/src/main/java/com/devwilly/tutorial/viewdraghelperex/MainActivity.java
+++ app/src/main/java/com/devwilly/tutorial/viewdraghelperex/MainActivity.java
@@ -1,7 +1,8 @@
 package com.devwilly.tutorial.viewdraghelperex;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -9,6 +10,14 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+
+        FirstVerticalFragment firstFragment = new FirstVerticalFragment();
+        SecondVerticalFragment secondFragment = new SecondVerticalFragment();
+
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.first_fragment_view, firstFragment)
+                .add(R.id.second_fragment_view, secondFragment).commit();
     }
 }
---
+++ app/src/main/java/com/devwilly/tutorial/viewdraghelperex/SecondVerticalFragment.java
@@ -0,0 +1,12 @@
+package com.devwilly.tutorial.viewdraghelperex;
+
+import android.support.v4.app.Fragment;
+
+
+/**
+ * Created by Willy on 2017/3/12.
+ */
+
+public class SecondVerticalFragment extends Fragment {
+
+}
