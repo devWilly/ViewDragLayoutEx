9edd87d787051d3ceff63f14b40c1566b401ec6a
Willy Chang <ms70831@gmail.com>
2017-03-12 18:05:33 +0800

    setting firstView and secondView layout position

--- app/src/main/java/com/devwilly/tutorial/viewdraghelperex/ui/ViewDragLayout.java
+++ app/src/main/java/com/devwilly/tutorial/viewdraghelperex/ui/ViewDragLayout.java
@@ -3,7 +3,9 @@ package com.devwilly.tutorial.viewdraghelperex.ui;
 import android.content.Context;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
+import android.support.v4.widget.ViewDragHelper;
 import android.util.AttributeSet;
+import android.view.View;
 import android.view.ViewGroup;
 
 
@@ -12,6 +14,9 @@ import android.view.ViewGroup;
  */
 
 public class ViewDragLayout extends ViewGroup {
+
+    private View mFirstView, mSecondView;
+    private int mFirstHeight;
 
     public ViewDragLayout(Context context) {
         super(context);
@@ -31,7 +36,34 @@ public class ViewDragLayout extends ViewGroup {
     }
 
     @Override
-    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+        mFirstView = getChildAt(0);
+        mSecondView = getChildAt(1);
+    }
 
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        if (mFirstView.getTop() == 0) {
+            mFirstView.layout(l, 0, r, b - t);
+            mSecondView.layout(l, 0, r, b - t);
+
+            mFirstHeight = mFirstView.getMeasuredHeight();
+            mSecondView.offsetTopAndBottom(mFirstHeight);
+        } else {
+            mFirstView.layout(l, mFirstView.getTop(), r, mFirstView.getBottom());
+            mSecondView.layout(l, mSecondView.getTop(), r, mSecondView.getBottom());
+        }
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        measureChildren(widthMeasureSpec, heightMeasureSpec);
+
+        int maxWidth = MeasureSpec.getSize(widthMeasureSpec);
+        int maxHeight = MeasureSpec.getSize(heightMeasureSpec);
+        setMeasuredDimension(
+                resolveSizeAndState(maxWidth, widthMeasureSpec, 0),
+                resolveSizeAndState(maxHeight, heightMeasureSpec, 0));
     }
 }
