8d0269bc7f9e4fe1e3fa45366a8976de3bbf39af
Willy Chang <ms70831@gmail.com>
2017-03-13 10:58:21 +0800

    create first fragment layout

--- app/src/main/java/com/devwilly/tutorial/viewdraghelperex/FirstVerticalFragment.java
+++ app/src/main/java/com/devwilly/tutorial/viewdraghelperex/FirstVerticalFragment.java
@@ -1,6 +1,11 @@
 package com.devwilly.tutorial.viewdraghelperex;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 
 /**
@@ -9,4 +14,11 @@ import android.support.v4.app.Fragment;
 
 public class FirstVerticalFragment extends Fragment {
 
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+            @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.first_fragment_layout, container, false);
+        return view;
+    }
 }
